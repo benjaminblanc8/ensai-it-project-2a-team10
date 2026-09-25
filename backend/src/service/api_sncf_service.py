1e859519e45759e1c6ad92da8f8bc957a5142457
AlexYu <alexandre.yu@eleve.ensai.fr>
2026-09-25 14:15:22 +0200

    modif api_sncfservice

--- backend/src/service/api_sncf_service.py
+++ backend/src/service/api_sncf_service.py
@@ -1,5 +1,6 @@
-import requests
 import os
+
+import requests
 from dotenv import load_dotenv
 
 # Load variables from .env
@@ -8,9 +9,7 @@ load_dotenv()
 API_KEY = os.getenv("SNCF_API_KEY")
 
 if not API_KEY:
-    raise RuntimeError(
-        "SNCF_API_KEY is missing. Add it to your .env file."
-    )
+    raise RuntimeError("SNCF_API_KEY is missing. Add it to your .env file.")
 
 BASE_URL = "https://api.sncf.com/v1/coverage/sncf/"
 
@@ -18,22 +17,21 @@ BASE_URL = "https://api.sncf.com/v1/coverage/sncf/"
 class ApiSncfService:
     def __init__(self):
         self.base_url = BASE_URL
-    
+
     def get_stops(self):
-        url = f"{BASE_URL}stop_points/"
-        
-        response = requests.get(
-            url,
-            auth=(API_KEY, "")
-        )
-        
+        url = f"{BASE_URL}stop_areas/"
+
+        response = requests.get(url, auth=(API_KEY, ""))
+
         response.raise_for_status()
-        
+
         data = response.json()
-         
+
         if response.status_code == 200:
             return data
         else:
             response.raise_for_status()
 
 
+# Pour obtenir une gare avec un nom précis, on peut utiliser l'endpoint "places" avec le paramètre de requête "q" pour spécifier le nom de la gare. Par exemple, pour obtenir des informations sur la gare d'Abancourt, on peut faire une requête GET à l'URL suivante :
+# https://api.sncf.com/v1/coverage/sncf/places?q=Abancourt&type[]=stop_area
